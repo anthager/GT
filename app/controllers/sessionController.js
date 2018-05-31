bea986602ae41b29b2f500c9c4a9b7a980e5422a
antonhager <anthage@student.chalmers.se>
2018-05-31 02:43:51 +0200

    removed commented out code

--- app/controllers/sessionController.js
+++ app/controllers/sessionController.js
@@ -29,40 +29,6 @@ function add({
 		return{code: 500, message: 'shits fucked up yo'}
 	})
 }
-// function add({
-// 	sender,
-// 	opponent,
-// 	amount
-// }) {
-// 	return new Promise(function(resolve, reject) {
-// 		User.getFormatedUsers(sender, opponent)
-// 		.then((users) => {
-// 			if (users[0].username !== sender){
-// 				amount *= -1
-// 			}
-
-// 			Promise.all([Session.add(users[0], users[1], amount), Debt.updateDebt(users[0], users[1], amount)])
-// 			.then((values) => {
-// 				resolve({
-// 					code: 200,
-// 					message : {
-// 						success: true, 
-// 						sessionInfo: `session between ${users[0].username} and ${users[1].username} for ${amount} saved`, 
-// 						debtInfo: `${users[0].username} and ${users[1].username} is now ${values[1].amount}`
-// 					}
-// 				})
-// 			})
-// 			.catch((err) => {
-// 				console.error(err)
-// 				reject({code: 500, message: 'shits fucked up yo'})
-// 			})
-// 		})
-// 		.catch((err) => {
-// 			console.error(err)
-// 			reject(err)
-// 		})
-// 	})
-// }
 
 module.exports = {
 	add: add
