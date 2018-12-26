70e4e6cc47053b0270dfa22b52bf7a8ffc8a29c3
anthager <anthage@student.chalmers.se>
2018-12-27 00:40:28 +0100

    edited serverless

--- serverless/index.js
+++ serverless/index.js
@@ -1,7 +1,3 @@
-const { parse } = require("url");
-
 module.exports = (req, res) => {
-  const { query } = parse(req.url, true);
-  const { name = "World" } = query;
-  res.end(`Hello ${name}!`);
-};
+    res.end(`Hello from Node.js on Now 2.0!`);
+  };
---
+++ serverless/index.ts
@@ -0,0 +1,3 @@
+export const handler = (req:any, res: any) => {
+	return res.send("hej")
+}
